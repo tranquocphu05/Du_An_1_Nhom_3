b56593a439898f04fd32dc2f694939e658a95b04
quocphudev05 <tranquocphu17102005@gmail.com>
2024-12-06 20:45:33 +0700

    infor

---
+++ modules/client/introduce/views/indexView.php
@@ -0,0 +1,77 @@
+<?php require "layout/client/header_client.php" ?>
+
+ <!----------------------------------------------------------------------------------------------------->
+ <hr class="display-4">
+    <!--Content-->
+    <!-- chính sách đổi trả -->
+    <div class="container-returnpolicy">
+        <div class="row-text-center">
+            <h1><a href="/du_an_1_Nhom3/?role=client">Trang chủ</a>/ <a href="/du_an_1_Nhom3/?role=client&mod=Privacy_Policy&action=index">Trang Giới Thiệu </a></h1>
+            <div class="col-text">
+                <div class="container-returnpolicy-right">
+                    <h1><a href="#">Bài viết</a></h1>
+                    <div class="posts">
+
+                        <p class="text">
+                            1. Giới thiệu
+
+                            Chào mừng quý khách hàng đến với website chúng tôi.
+
+                            Khi quý khách hàng truy cập vào trang website của chúng tôi có nghĩa là quý khách đồng ý với
+                            các điều khoản này. Trang web có quyền thay đổi, chỉnh sửa, thêm hoặc lược bỏ bất kỳ phần
+                            nào trong Điều khoản mua bán hàng hóa này, vào bất cứ lúc nào. Các thay đổi có hiệu lực ngay
+                            khi được đăng trên trang web mà không cần thông báo trước. Và khi quý khách tiếp tục sử dụng
+                            trang web, sau khi các thay đổi về Điều khoản này được đăng tải, có nghĩa là quý khách chấp
+                            nhận với những thay đổi đó.
+                            <br>
+
+                            Quý khách hàng vui lòng kiểm tra thường xuyên để cập nhật những thay đổi của chúng tôi.
+                            <img src="image/banner3.png" alt="">
+
+                            Chào mừng quý khách hàng đến với website chúng tôi.
+                            <br>
+
+                            Khi quý khách hàng truy cập vào trang website của chúng tôi có nghĩa là quý khách đồng ý với
+                            các điều khoản này. Trang web có quyền thay đổi, chỉnh sửa, thêm hoặc lược bỏ bất kỳ phần
+                            nào trong Điều khoản mua bán hàng hóa này, vào bất cứ lúc nào. Các thay đổi có hiệu lực ngay
+                            khi được đăng trên trang web mà không cần thông báo trước. Và khi quý khách tiếp tục sử dụng
+                            trang web, sau khi các thay đổi về Điều khoản này được đăng tải, có nghĩa là quý khách chấp
+                            nhận với những thay đổi đó.
+                            <br>
+
+                            Quý khách hàng vui lòng kiểm tra thường xuyên để cập nhật những thay đổi của chúng tôi.
+                            <img src="image/banner3.png" alt="">
+
+                        </p>
+                    </div>
+                </div>
+                <div class="container-returnpolicy-left">
+                    <h1><a href="#">Danh mục</a></h1>
+                    <div class="category">
+                        <h2><a href="#">Trang chủ</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=introduce&action=index">Giới thiệu</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=news&action=index">Tin tức</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=contact&action=index">Liên hệ</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=return_Policy&action=index">Chính sách đổi trả</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=Privacy_Policy&action=index">Chính sách bảo mật</a></h2>
+                        <hr>
+                        <h2><a href="/du_an_1_Nhom3/?role=client&mod=service_Policy&action=index">Chính sách dịch vụ</a></h2>
+                        <hr>
+                        <img src="image/banner3.png" alt="">
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- end chính sách đổi trả             -->
+    
+<?php require "layout/client/footer_client.php" ?>
+
+
