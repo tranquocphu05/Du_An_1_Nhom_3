7a16d4b6ecb7b144ced9bd0a3f834237d7f241f0
quocphudev05 <tranquocphu17102005@gmail.com>
2024-12-07 22:16:46 +0700

    TranQuocPhu

---
+++ mail/index.php
@@ -0,0 +1,40 @@
+<?php
+function GuiMail($title, $content, $addressMail){   
+    require "PHPMailer/src/PHPMailer.php"; 
+    require "PHPMailer/src/SMTP.php"; 
+    require 'PHPMailer/src/Exception.php'; 
+    $mail = new PHPMailer\PHPMailer\PHPMailer(true);//true:enables exceptions
+    try {
+        $mail->SMTPDebug = 1; //: chế độ debug. khi chạy ngon thì chỉnh lại  nhé
+        $mail->isSMTP();  
+        $mail->CharSet  = "utf-8";
+        $mail->Host = 'smtp.gmail.com';  //SMTP servers
+        $mail->SMTPAuth = true; // Enable authentication
+        $mail->Username = 'tranquocphu.05@gmail.com'; // SMTP username
+        $mail->Password = 'uavz cssw teag lcwh';   // SMTP password
+        $mail->SMTPSecure = 'ssl';  // encryption TLS/SSL 
+        $mail->Port = 465;  // port to connect to                
+        $mail->setFrom('tranquocphu.05@gmail.com', 'hottel' ); 
+
+        $mail->addAddress($addressMail); //mail và tên người nhận  
+        $mail->isHTML(true);  
+      
+        $mail->Subject = $title;
+        $mail->Body = $content;
+        $mail->smtpConnect( array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+                "allow_self_signed" => true
+            )
+        ));
+        $mail->send();
+        echo 'Đã gửi mail xong';
+    } catch (Exception $e) {
+        echo 'Mail không gửi được. Lỗi: ', $mail->ErrorInfo;
+    }
+ }//function GuiMail
+
+
+?>
+
