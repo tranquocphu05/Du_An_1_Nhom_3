8f7f1cb78e68dc0159fff474d1f4d1babab0e4e6
Duyy-github <phamduy1612@gmail.com>
2024-12-08 15:59:22 +0700

    feat:register

---
+++ modules/admin/comment/controllers/indexController.php
@@ -0,0 +1,16 @@
+<?php
+function construct(){
+    load_model("index");
+}
+
+function indexAction(){
+    $data['comment']=manage_comments();
+    load_view('index', $data);
+}
+function detailcommentAction(){
+    request_auth(true);
+    $id=$_GET['id'];
+    $data['comment']=get_list_comments($id);
+    load_view('detailcomment',$data);
+}
+?>
