15865dcff171594f49bb265bf78c42cbb9a98e07
Duyy-github <phamduy1612@gmail.com>
2024-12-09 14:30:39 +0700

    feat:register

---
+++ modules/client/home/controllers/indexController.php
@@ -0,0 +1,18 @@
+<?php
+
+function construct() {
+    load_model('index');
+}
+
+
+function indexAction() {
+    // đẩy phòng lên giao diện trang chủ
+    $data['productions'] = get_list_productions();
+
+    // đẩy phẩn dịch vụ lên trang chủ
+    $data['services'] = get_list_services();
+    $data['categories'] = get_list_categories();
+// chuyển sang trang chủ với dữ liệu trên
+    load_view('index', $data);
+}
+
